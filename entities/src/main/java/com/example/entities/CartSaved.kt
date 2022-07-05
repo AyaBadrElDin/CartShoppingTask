c513dcf24d4d9ab2408118a3745ebabb2cd16485
Aya Mahmoud <aya_conan_1992@yahoo.com>
2022-07-05 12:43:20 +0200

    - Add library module for entities,business. - add entities class

---
+++ entities/src/main/java/com/example/entities/CartSaved.kt
@@ -0,0 +1,10 @@
+package com.example.entities
+
+import java.util.ArrayList
+
+class CartSaved {
+    var productList: ArrayList<Product>? = null
+    var firstDateCartAdded: Int? = null
+    var dateAfter3Days: Int? = null
+
+}
